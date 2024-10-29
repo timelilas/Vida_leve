030022da123527d92e6d2e09c2c2352200cba76d
Nalbert Cerqueira <nalbertc.p@gmail.com>
2024-10-28 21:48:24 -0300

    refactor: inicando o servidor somente depois de inicializar o banco

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import App from './app';
-import connectDB from './database'; // Certifique-se de que o caminho está correto
+import { connectDB } from './database/index'; // Certifique-se de que o caminho está correto
 
 // Defina a porta do servidor
-const PORT = process.env.PORT || 3000;
-
-// Inicialize a conexão com o banco de dados
-connectDB();
+const PORT = process.env.SERVER_PORT || 3000;
 
 // Inicialize e inicie o servidor
 const appInstance = new App();
-appInstance.start(PORT);
+
+// Inicialize a conexão com o banco de dados e o servidor
+connectDB().then(()=>appInstance.start(PORT));
+
