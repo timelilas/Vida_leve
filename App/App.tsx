796ebb5ab444cf062b39f702b2551b5b51f57ce4
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-04 15:38:31 -0300

    refactor: carregando as fontes em App.jsx
    - Criação do hook useAppFonts para carregar as fontes do projeto
    - Importação do "@expo/metro-runtime" em App.jsx para habilitar
    o hot reload no browser

--- App/App.tsx
+++ App/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import Navigation from './src/Navigation';
+import React from "react";
+import Navigation from "./src/Navigation";
+import * as SplashScreen from "expo-splash-screen";
+import "@expo/metro-runtime";
+import { useAppFonts } from "./src/hooks/useAppFonts";
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
-  return (
-    <Navigation />
-  );
+  const fontsLoaded = useAppFonts();
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Navigation />;
 };
 
 export default App;
