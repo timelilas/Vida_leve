70484fff28547abb30f399287ca14ecd4ffa4800
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-04 15:42:53 -0300

    feat: criação de botões que serão utilizados ao longo da aplicação

---
+++ App/src/components/buttons/GoalButtons/ModeratedGoalButton.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from "react-native";
+import { ToggleButton, ToggleButtonProps } from "../ToggleButton";
+import { useGoalButtonStyles } from "./styles";
+import { WindIcon } from "../../icons/WindIcon";
+
+export function ModeratedGoalButton(
+  props: Omit<ToggleButtonProps, "children">
+) {
+  const goalButtonStyles = useGoalButtonStyles();
+
+  return (
+    <ToggleButton onPress={props.onPress} selected={props.selected}>
+      <View style={goalButtonStyles.contentContainer}>
+        <View style={goalButtonStyles.titleContainer}>
+          <WindIcon />
+          <View>
+            <Text style={goalButtonStyles.subtitle}>Progresso Moderado</Text>
+          </View>
+        </View>
+        <View>
+          <View style={goalButtonStyles.weekGoal}>
+            <Text style={goalButtonStyles.weekTarget}>2000 kcal/dia</Text>
+            <Text style={goalButtonStyles.weekPeriod}>por 11 semanas</Text>
+          </View>
+        </View>
+      </View>
+    </ToggleButton>
+  );
+}
---
+++ App/src/components/buttons/GoalButtons/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet, useWindowDimensions } from "react-native";
+
+export function useGoalButtonStyles() {
+  const screen = useWindowDimensions();
+
+  return StyleSheet.create({
+    contentContainer: {
+      flexDirection: screen.width >= 430 ? "row" : "column",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: screen.width >= 430 ? 0 : 8,
+    },
+    titleContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 6,
+    },
+    subtitle: {
+      width: "100%",
+      fontSize: 16,
+      fontFamily: "Roboto-400",
+      color: "#242424",
+    },
+    weekGoal: {
+      alignItems: screen.width >= 430 ? "flex-end" : "center",
+    },
+    weekTarget: {
+      fontSize: 24,
+      color: "#242424",
+      fontFamily: "Roboto-700",
+    },
+    weekPeriod: {
+      fontSize: 14,
+      fontFamily: "Roboto-300",
+      color: "#242424",
+    },
+  });
+}
