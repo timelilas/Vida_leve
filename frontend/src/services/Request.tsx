3c12da09fa0fd83582bbed49da4a0ae9f9b64283
Higo Jhon <higojhon98@gmail.com>
2024-11-05 22:06:27 -0300

    FEAT: Criando chamada da API

---
+++ frontend/src/services/Request.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://vida-leve-h7ug.onrender.com',
+    timeout: 5000,
+});
+
+export const request = async (method: string, endpoint: string, data = null) => {
+    try {
+        const response = await api({
+            method,
+            url: endpoint,
+            data,
+        })
+
+        return response.data
+    } catch (error) {
+        console.error(`Erro ao fazer ${method.toUpperCase()} na rota ${endpoint}:`, error);
+        throw error;
+    }
+}
