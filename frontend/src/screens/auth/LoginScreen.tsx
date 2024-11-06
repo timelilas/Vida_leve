2c21a3cebadadcb4a510e666fda0d9f7b038b69c
Higo Jhon <higojhon98@gmail.com>
2024-11-05 22:24:42 -0300

    FEAT: Implementando a chamada na API na Tela de LoginScreen

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -7,10 +7,22 @@ import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
 import { ScreenWrapper } from "../../components/ScreenWrapper";
 import { SubmitButton } from "../../components/buttons/SubmitButton";
+import { request } from "../../services/Request";
 
 const LoginScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      const user = { email, senha: password };
+      const { data } = await request("POST", "/auth/login", user )
+      console.log(data);
+      navigation.navigate("Onboarding/Goals")
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <ScreenWrapper>
@@ -40,7 +52,7 @@ const LoginScreen = ({ navigation }: { navigation: NavigationProp<any> }) => {
           style={styles.button}
           title="Continuar"
           type="primary"
-          onPress={() => navigation.navigate("Onboarding/Goals")}
+          onPress={ login }
         />
       </ScrollView>
     </ScreenWrapper>
