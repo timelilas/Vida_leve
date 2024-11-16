c50b6ec42fd3ba829a555e1d4e95cf063560cec1
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-16 18:22:10 -0300

    perf: componente Calendar foi envolvido na função memo para evitar renderizações desnecessárias

--- frontend/src/components/calendar/Calendar.tsx
+++ frontend/src/components/calendar/Calendar.tsx
@@ -5,6 +5,7 @@ import { CalendarHeader } from "./CalendarHeader";
 import { CalendarSection } from "./types";
 import { CustomCalendarBody } from "../../libs/react-native-calendars/CustomCalendarBody";
 import { CalendarFooterButton } from "./CalendarFooterButton";
+import { memo } from "react";
 
 interface CalendarProps {
   month: number;
@@ -24,53 +25,63 @@ interface CalendarProps {
   confirm: () => void;
 }
 
-export function Calendar(props: CalendarProps) {
-  if (props.section !== "calendar") {
-    return (
-      <CalendarMenu
-        section={props.section}
-        month={props.month}
-        year={props.year}
-        selectMonth={props.setMonth}
-        selectYear={props.setYear}
-        selectSection={props.setSection}
-      />
-    );
-  }
-
-  return (
-    <View style={styles.container}>
-      <CalendarHeader
-        incrementMonth={props.incrementMonth}
-        decrementMonth={props.decrementMonth}
-        incrementYear={props.incrementYear}
-        decrementYear={props.decrementYear}
-        selectMenu={props.setSection}
-        month={props.month}
-        year={props.year}
-      />
-      <CustomCalendarBody
-        key={`${props.year}-${props.month}`}
-        targetDate={props.currentDate}
-        onDayPress={(date) => {
-          props.setCurrentDate(date);
-          props.setMonth(date.month);
-          props.setYear(date.year);
-        }}
-        current={`${props.year}-${props.month}`}
-      />
-      <View style={styles.footer}>
-        <CalendarFooterButton
-          onPress={props.clear}
-          label="Limpar"
-          style={styles.leftFooterButton}
+export const Calendar = memo(
+  (props: CalendarProps) => {
+    if (props.section !== "calendar") {
+      return (
+        <CalendarMenu
+          section={props.section}
+          month={props.month}
+          year={props.year}
+          selectMonth={props.setMonth}
+          selectYear={props.setYear}
+          selectSection={props.setSection}
         />
-        <CalendarFooterButton onPress={props.close} label="Cancelar" />
-        <CalendarFooterButton onPress={props.confirm} label="Ok" />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <CalendarHeader
+          incrementMonth={props.incrementMonth}
+          decrementMonth={props.decrementMonth}
+          incrementYear={props.incrementYear}
+          decrementYear={props.decrementYear}
+          selectMenu={props.setSection}
+          month={props.month}
+          year={props.year}
+        />
+        <CustomCalendarBody
+          key={`${props.year}-${props.month}`}
+          targetDate={props.currentDate}
+          onDayPress={(date) => {
+            props.setCurrentDate(date);
+            props.setMonth(date.month);
+            props.setYear(date.year);
+          }}
+          current={`${props.year}-${props.month}`}
+        />
+        <View style={styles.footer}>
+          <CalendarFooterButton
+            onPress={props.clear}
+            label="Limpar"
+            style={styles.leftFooterButton}
+          />
+          <CalendarFooterButton onPress={props.close} label="Cancelar" />
+          <CalendarFooterButton onPress={props.confirm} label="Ok" />
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  },
+  (prev, next) => {
+    const equalYears = prev.year === next.year;
+    const equalMonths = prev.month === next.month;
+    const equalDates = prev.currentDate === next.currentDate;
+    const equalSections = prev.section === next.section;
+
+    return equalYears && equalMonths && equalDates && equalSections;
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
