fb98fcf00b4e399d1aa72fd7810c45a633947ca1
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-22 23:43:18 -0300

    refactor: correção na máscara de telefone

--- frontend/src/utils/validations/phone/index.ts
+++ frontend/src/utils/validations/phone/index.ts
@@ -1,8 +1,13 @@
 import { ValidationResult } from "../type";
+import { defaultMissingFieldMsg } from "../variables";
 import { invalidPhoneFormatMsg } from "./variables";
 
 export function validatePhone(phone: string): ValidationResult {
   const onlyNumbers = phone.replace(/\D/g, "");
+
+  if (phone.length === 0) {
+    return { success: false, error: defaultMissingFieldMsg };
+  }
 
   if (onlyNumbers.length !== 11) {
     return { success: false, error: invalidPhoneFormatMsg };
