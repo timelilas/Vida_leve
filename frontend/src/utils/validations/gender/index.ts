83f96a7b75445ef4105cf90f16a91faddfece53c
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-23 00:20:51 -0300

    refactor: adição de uma mensagem mais amigável para o usuário na validação do gênero

--- frontend/src/utils/validations/gender/index.ts
+++ frontend/src/utils/validations/gender/index.ts
@@ -1,10 +1,12 @@
 import { ValidationResult } from "../type";
-import { defaultMissingFieldMsg } from "../variables";
 import { allowedGenders } from "./variables";
 
 export function validateGender(gender: string): ValidationResult {
   if (!gender.length) {
-    return { success: false, error: defaultMissingFieldMsg };
+    return {
+      success: false,
+      error: "Ops! Você precisa escolher uma das opções acima para continuar.",
+    };
   }
 
   if (!allowedGenders.includes(gender)) {
