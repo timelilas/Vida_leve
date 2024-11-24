eb24e70c8ca6d9fddaa83de897c7cccee094f8e6
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-11-24 12:12:55 -0300

    refactor: habilitando useForm para reconhecer erros de conexão

--- frontend/src/services/helpers.ts
+++ frontend/src/services/helpers.ts
@@ -2,7 +2,7 @@ import { FailedHttpResponse } from "./types";
 
 export function failedConnectionError(): FailedHttpResponse["error"] {
   return {
-    field: null,
+    field: "connection",
     message:
       "Ops! Parece que houve uma falha na conexão, tente novamente mais tarde.",
   };
