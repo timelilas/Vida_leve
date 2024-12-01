7d223581928d159fcf2fe31f2cc72bb165e3e50f
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-12-01 18:59:12 -0300

    feat: setup do zustand e criação das stores

--- frontend/src/@core/progress/progress.ts
+++ frontend/src/@core/progress/progress.ts
@@ -1,7 +1,6 @@
 export type ActitivyFrequency = "pouca" | "leve" | "moderada" | "intensa";
 
 export interface ProgressProps {
-  id: number;
   height: number;
   weight: number;
   goalWeight: number;
