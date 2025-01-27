9af3e0b220d9f2873aefe63a586c036274d8bc99
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-01-27 16:16:08 -0300

    chore: uso do componente ProgressForm em ProgressFormScreen

--- frontend/src/screens/onboarding/progressForm/ProgressFormScreen.tsx
+++ frontend/src/screens/onboarding/progressForm/ProgressFormScreen.tsx
@@ -1,38 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { ScreenWrapper } from "../../../components/ScreenWrapper";
 import { ScreenHeader } from "../../../components/ScreenHeader";
-import { Input } from "../../../components/inputs/Input";
-import { ProgressFormData } from "./types";
-import { ActitivyFrequency } from "../../../@core/entities/@shared/activityFrequency/type";
-import { SubmitButton } from "../../../components/buttons/SubmitButton";
 import { ScreenTitle } from "../../../components/ScreenTitle";
 import { Paragraph } from "../../../components/Paragraph";
-import { useForm } from "../../../hooks/useForm";
-import { validateHeight } from "../../../utils/validations/height";
-import { validateWeight } from "../../../utils/validations/weight";
-import { validateActitivyFrequency } from "../../../utils/validations/activityFrequency";
-import { missingProfileFormField } from "./utils";
-import { validateGoalWeight } from "../../../utils/validations/goalWeight";
 import { useUserStore } from "../../../store/user";
 import { useProgressStore } from "../../../store/progress";
-import { calculateAge } from "../../../@core/entities/user/helpers";
-import { HttpError } from "../../../@core/errors/httpError";
 import { ConnectionError } from "../../../@core/errors/connectionError";
-import { buildCaloriePlan } from "../../../@core/entities/caloriePlan/helpers";
 import { useCaloriePlanStore } from "../../../store/caloriePlan";
-import {
-  maskHeight,
-  heightToString,
-  onlyNumbers,
-  parseHeight,
-} from "../../../utils/masks";
-import { validPlanTypes } from "../../../@core/entities/@shared/planType/constants";
+import { heightToString } from "../../../utils/masks";
 import { useAppNavigation } from "../../../hooks/useAppNavigation";
 import { RouteConstants } from "../../../routes/types";
 import { httpProgressService } from "../../../services/progress";
 import { useSnackbar } from "../../../hooks/useSnackbar";
-import { ActivityFrequencySelection } from "../../../components/acitivityFrequencySelection/ActivityFrequencySelection";
-import { WeightInput } from "../../../components/inputs/WeightInput";
+import { ProgressForm } from "../../../components/progressForm/ProgressForm";
+import { OnProgressSubmitData } from "../../../components/progressForm/types";
 
 const ProgressFormScreen = () => {
   const { Snackbar, showSnackbar } = useSnackbar();
@@ -41,26 +22,19 @@ const ProgressFormScreen = () => {
   const progress = useProgressStore((state) => state.data);
   const gender = useUserStore((state) => state.data.gender);
   const birthDate = useUserStore((state) => state.data.birthDate);
+  const navigation = useAppNavigation();
 
-  const { data, handleChange, setError, validateField, onSubmit } =
-    useForm<ProgressFormData>({
-      initialState: {
-        height: progress?.height ? heightToString(progress?.height) : "",
-        weight: progress?.weight ?? 0,
-        goalWeight: progress?.goalWeight ?? 0,
-        activityFrequency: progress?.activityFrequency ?? null,
-      },
-    });
-  const { values, error, isSubmitting } = data;
-  const { height, weight, goalWeight, activityFrequency } = values;
-  const navigation = useAppNavigation({ preventGoBack: isSubmitting });
+  const initialFormData = {
+    height: progress?.height ? heightToString(progress?.height) : "",
+    weight: progress?.weight ?? 0,
+    goalWeight: progress?.goalWeight ?? 0,
+    activityFrequency: progress?.activityFrequency ?? null,
+    gender: gender,
+    birthDate: birthDate,
+  };
 
   function goBack() {
     navigation.goBack();
-  }
-
-  function showErrorSnackbar(message: string) {
-    showSnackbar({ duration: 4000, message: message, variant: "error" });
   }
 
   function navigateToPlanSelection() {
@@ -69,108 +43,27 @@ const ProgressFormScreen = () => {
     });
   }
 
-  function selectActitivyFrequency(frequency: ActitivyFrequency) {
-    const newFrequency = activityFrequency === frequency ? null : frequency;
-    handleChange("activityFrequency", newFrequency);
-    validateField(
-      "activityFrequency",
-      newFrequency || "",
-      validateActitivyFrequency
-    );
-  }
-
-  function generateCaloriePlans() {
-    if (!birthDate || !gender || !activityFrequency) {
-      return [];
-    }
-
-    const age = calculateAge(new Date(birthDate as string));
-    const userData = { weight, height: parseHeight(height), gender, age };
-    const goalData = { dailyActivityLevel: activityFrequency, goalWeight };
-    const planList = validPlanTypes.map((plan) =>
-      buildCaloriePlan({ ...userData, ...goalData, planType: plan })
+  async function onSubmit(data: OnProgressSubmitData) {
+    const { formData, newCaloriePlans } = data;
+    const { data: responseData } = await httpProgressService.createProgress(
+      formData
     );
 
-    return planList;
-  }
-
-  function handleGoalWeightValidation() {
-    if (!birthDate || !gender) {
-      return showErrorSnackbar(missingProfileFormField);
-    }
-
-    const { success: validWeight } = validateWeight(weight);
-    const { success: validHeight } = validateHeight(parseHeight(height));
-
-    if (validWeight && validHeight) {
-      validateField("goalWeight", goalWeight, (goalWeight: number) =>
-        validateGoalWeight({
-          gender,
-          age: calculateAge(new Date(birthDate)),
-          height: parseHeight(height),
-          weight,
-          goalWeight,
-        })
-      );
-    }
-  }
-
-  function validateAllFields() {
-    if (!birthDate || !gender) {
-      return showErrorSnackbar(missingProfileFormField);
-    }
-
-    const age = calculateAge(new Date(birthDate));
-    const heightAsNumber = parseHeight(height);
-    const goalWeightParams = {
-      height: heightAsNumber,
-      weight,
-      gender,
-      goalWeight,
-      age,
-    };
-
-    const validationMap = {
-      height: validateHeight(heightAsNumber),
-      weight: validateWeight(weight),
-      goalWeight: validateGoalWeight(goalWeightParams),
-      activityFrequency: validateActitivyFrequency(activityFrequency || ""),
-    };
-
-    for (const [field, validation] of Object.entries(validationMap)) {
-      if (!validation.success) {
-        setError({ field: field as any, message: validation.error });
-        return false;
-      }
-    }
-    return true;
-  }
-
-  async function handleSubmit() {
-    if (!validateAllFields()) return;
-
-    const { data } = await httpProgressService.createProgress({
-      weight,
-      goalWeight,
-      height: parseHeight(height),
-      activityFrequency: activityFrequency as ActitivyFrequency,
-    });
-
-    const plans = generateCaloriePlans();
-    setProgress(data);
-    setPlans(plans);
+    setProgress(responseData);
+    setPlans(newCaloriePlans);
     navigateToPlanSelection();
   }
 
-  function handleError(error: Error) {
+  function onError(error: Error) {
     if (error instanceof ConnectionError) {
       return navigation.navigate(RouteConstants.ConnectionError);
     }
-    if (error instanceof HttpError) {
-      setError({ field: error.field as any, message: error.message });
-    }
     if (!(error as any).field) {
-      showErrorSnackbar(error.message);
+      showSnackbar({
+        duration: 4000,
+        message: error.message,
+        variant: "error",
+      });
     }
   }
 
@@ -182,65 +75,11 @@ const ProgressFormScreen = () => {
         Precisamos da sua altura, peso atual, meta de peso e frequência de
         atividade física para personalizar sua jornada.
       </Paragraph>
-      <View style={styles.form}>
-        <View style={styles.inputWrapper}>
-          <Input
-            onChangeText={(value) => handleChange("height", maskHeight(value))}
-            onBlur={() =>
-              validateField("height", parseHeight(height), validateHeight)
-            }
-            disabled={isSubmitting}
-            error={error.field === "height"}
-            value={`${height}`}
-            errorMessage={error.field === "height" ? error.message : undefined}
-            keyboardType="numeric"
-            label="Altura"
-            placeholder="Ex.: 1,60"
-            autoFocus
-          />
-          <WeightInput
-            label="Peso atual"
-            value={`${weight || ""}`}
-            disabled={isSubmitting}
-            placeholder="Ex.: 60 kg"
-            error={error.field === "weight"}
-            errorMessage={error.field === "weight" ? error.message : undefined}
-            onBlur={() => validateField("weight", weight, validateWeight)}
-            onChangeText={(value) =>
-              handleChange("weight", parseInt(onlyNumbers(value, 3)))
-            }
-          />
-          <WeightInput
-            label="Peso desejado"
-            value={`${goalWeight || ""}`}
-            disabled={isSubmitting}
-            placeholder="Ex.: 55 kg"
-            error={error.field === "goalWeight"}
-            errorMessage={
-              error.field === "goalWeight" ? error.message : undefined
-            }
-            onBlur={handleGoalWeightValidation}
-            onChangeText={(value) =>
-              handleChange("goalWeight", parseInt(onlyNumbers(value, 3)))
-            }
-          />
-        </View>
-        <ActivityFrequencySelection
-          style={styles.planSelection}
-          onSelect={selectActitivyFrequency}
-          selectedFrequency={activityFrequency}
-          disabled={isSubmitting}
-          errorMessage={
-            error.field === "activityFrequency" ? error.message : undefined
-          }
-        />
-      </View>
-      <SubmitButton
-        disabled={isSubmitting}
-        onPress={onSubmit(handleSubmit, handleError)}
-        style={styles.submitButton}
-        type="primary"
-        title="Continuar cadastro"
+      <ProgressForm
+        variant="default"
+        initialData={initialFormData}
+        onError={onError}
+        onSubmit={onSubmit}
       />
     </ScreenWrapper>
   );
@@ -257,25 +96,5 @@ const styles = StyleSheet.create({
   },
   description: {
     marginTop: 8,
-  },
-  label: {
-    lineHeight: 16,
-  },
-  form: {
-    marginTop: 24,
-    marginBottom: 40,
-  },
-  inputWrapper: {
-    gap: 16,
-  },
-  planSelection: {
-    marginTop: 32,
-  },
-  wrapper: {
-    gap: 8,
-    paddingBottom: 16,
-  },
-  submitButton: {
-    marginTop: "auto",
   },
 });
