aa90469811b81c2ed9fb390c017fa9f904afc1e3
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-02-11 17:39:33 -0300

    chore: criação do serviço HttpFoodService

---
+++ frontend/src/services/food/HttpFoodService.ts
@@ -0,0 +1,14 @@
+import { FoodProps } from "../../@core/entities/food/type";
+import { HttpService } from "../HttpService";
+import { HttpSearchFoodsInputDTO } from "./types";
+
+export class HttpFoodService extends HttpService {
+  public async searchFoods(params: HttpSearchFoodsInputDTO) {
+    const { name, limit, offset } = params;
+
+    return await this.submit<{ foods: FoodProps[]; hasMore: boolean }>({
+      method: "GET",
+      path: `/foods?name=${name}&limit=${limit}&offset=${offset}`,
+    });
+  }
+}
--- frontend/src/services/user/HttpUserService.ts
+++ frontend/src/services/user/HttpUserService.ts
@@ -8,13 +8,13 @@ import { STORAGE_ACCESS_TOKEN } from "../../constants/localStorageConstants";
 import { SecureStorage } from "../secureStorage/SecureStorage";
 
 export class HttpUserService extends HttpService {
-  public async updateProfile(data: HttpUpdateProfileInputDTO) {
+  public async updateProfile(params: HttpUpdateProfileInputDTO) {
     const accessToken = await SecureStorage.getItem(STORAGE_ACCESS_TOKEN);
 
     return await this.submit<HttpUpdateProfileOutputDTO>({
       method: "PUT",
       path: "/user/profile",
-      body: data,
+      body: params,
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
