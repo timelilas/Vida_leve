b4d4fbab90154d18b22369a2965f58a820a7cce3
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-02-18 14:03:06 -0300

    chore: alteração da pasta 'meal' para 'mealType'

--- frontend/src/@core/entities/@shared/mealType/constants.ts
+++ frontend/src/@core/entities/@shared/mealType/constants.ts
@@ -1,7 +1,12 @@
-const validMeals = [
+const validMealTypes = [
   "cafe-da-manha",
   "lanche",
   "almoco",
   "jantar",
   "outro",
 ] as const;
+
+interface Meal {
+  id: string;
+  type: "jantar";
+}
