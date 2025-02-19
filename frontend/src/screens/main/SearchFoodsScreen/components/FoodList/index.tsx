230106266b9ba40fc7f7534ced052d5848c87191
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-02-19 11:41:49 -0300

    styles: ajustes em alguns estilos das telas CreateMealScreen e SearchFoodsScreen

--- frontend/src/screens/main/SearchFoodsScreen/components/FoodList/index.tsx
+++ frontend/src/screens/main/SearchFoodsScreen/components/FoodList/index.tsx
@@ -3,16 +3,28 @@ import { styles } from "./styles";
 import { FoodItem } from "../FoodItem";
 import { FoodProps } from "../../../../../@core/entities/food/type";
 import { memo } from "react";
+import { useAppNavigation } from "../../../../../hooks/useAppNavigation";
+import { RouteConstants } from "../../../../../routes/types";
 
 interface FoodListProps {
   foods: FoodProps[];
 }
 
 export const FoodList = memo(({ foods }: FoodListProps) => {
+  const navigation = useAppNavigation();
+
+  function navigateToMealRegistration() {
+    navigation.navigate(RouteConstants.MealRegistration);
+  }
+
   return (
     <View style={styles.foodList}>
       {foods.map((food) => (
-        <FoodItem key={food.id} action={() => {}} name={food.name} />
+        <FoodItem
+          key={food.id}
+          action={navigateToMealRegistration}
+          name={food.name}
+        />
       ))}
     </View>
   );
