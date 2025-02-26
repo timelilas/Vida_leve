b9eb37d6273de5257ef77638b53db74fa5f2f651
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-02-25 21:42:35 -0300

    chore: adição de novos pacotes e configurações
    - Configuração do pacote react-native-skia para funcionar corretamente
    no browser e expo go.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./src/routes/AppRouter";
 import * as SplashScreen from "expo-splash-screen";
 import { useAppFonts } from "./src/hooks/useAppFonts";
-import { LogBox } from "react-native";
+import { LogBox, Platform } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./src/libs/react-query/queryClient";
+import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
 import "@expo/metro-runtime";
 
 SplashScreen.preventAutoHideAsync();
 LogBox.ignoreAllLogs(true);
 
 const App = () => {
-  const fontsLoaded = useAppFonts();
+  const { isFontsLoaded, error } = useAppFonts();
+  const [isSkiaLoaded, setIsSkiaLoaded] = useState(false);
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    if (isFontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [isFontsLoaded, error]);
+
+  //Loading skia .wasm file to enable skia canvas on web
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      LoadSkiaWeb({
+        locateFile: () => "/canvaskit.wasm",
+      }).then(() => setIsSkiaLoaded(true));
+    } else {
+      setIsSkiaLoaded(true);
+    }
+  }, []);
+
+  if (!isFontsLoaded || !isSkiaLoaded) {
     return null;
   }
 
