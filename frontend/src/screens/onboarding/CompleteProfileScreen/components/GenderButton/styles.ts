b493754444d7b771da954f7e98024547e626d448
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-04-07 17:11:28 -0300

    chore: aplicação do 'ProfileForm' na tela 'CompleteProfileScreen'

--- frontend/src/screens/onboarding/CompleteProfileScreen/components/GenderButton/styles.ts
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-  containerDisabled: {
-    opacity: 0.5,
-  },
-});
--- frontend/src/screens/onboarding/CompleteProfileScreen/styles.ts
+++ frontend/src/screens/onboarding/CompleteProfileScreen/styles.ts
@@ -1,43 +1,10 @@
 import { StyleSheet } from "react-native";
-import { fonts } from "../../../styles/fonts";
-import { colors } from "../../../styles/colors";
 
 export const styles = StyleSheet.create({
   title: {
     marginTop: 32,
   },
   text: {
-    marginTop: 8,
-  },
-  form: {
-    marginTop: 32,
-    gap: 16,
-    marginBottom: 56,
-  },
-  genderButtons: {
-    alignSelf: "flex-start",
-    flexDirection: "row",
-    gap: 40,
-  },
-  genderLabel: {
-    fontFamily: fonts.robotoRegular,
-    fontSize: 16,
-    lineHeight: 16,
-    color: colors.text.primary,
-    marginBottom: 8,
-  },
-  gender: {
-    fontSize: 16,
-    lineHeight: 16,
-    paddingVertical: 14,
-    paddingHorizontal: 6,
-    fontFamily: fonts.robotoRegular,
-    color: colors.text.primary,
-  },
-  submitButton: {
-    marginTop: "auto",
-  },
-  genderError: {
     marginTop: 8,
   },
 });
