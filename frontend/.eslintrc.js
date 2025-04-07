3bb3e73d3293f2b815437e4e65779f490ffb8503
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-04-07 17:30:40 -0300

    chore: integração do eslint e prettier com o frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  ignorePatterns: ["/dist/*"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: "explicit",
+        overrides: {
+          accessors: "explicit",
+          constructors: "off",
+          methods: "explicit",
+          parameterProperties: "off",
+          properties: "explicit",
+        },
+      },
+    ],
+  },
+};
