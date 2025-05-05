366436c87133589d222c2e7485b7b9839ae226cc
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-05-05 19:25:55 -0300

    chore: ajustes de identação

--- backend/src/database/models/WeightHistory.ts
+++ backend/src/database/models/WeightHistory.ts
@@ -1,9 +1,9 @@
-import { 
+import {
   CreationOptional,
   ForeignKey,
   InferAttributes,
   InferCreationAttributes,
-  Model
+  Model,
 } from "sequelize";
 import Sequelize from "sequelize";
 import { sequelize } from "../index";
@@ -12,51 +12,50 @@ import { WeightHistoryEntity } from "../../@core/entity/WeightHistoryEntity/enti
 import { TableNames } from "../constants";
 
 class WeightHistory extends Model<
-    InferAttributes<WeightHistory>,
-    InferCreationAttributes<WeightHistory>>
-{
-    declare id: CreationOptional<number>;
-    declare userId: ForeignKey<User["id"]>;
-    declare weight: number;
-    declare date: string;
-    
-    public toJSON(): WeightHistoryEntity {
-        const props = super.get();
-        return {
-            id: props.id,
-            userId: props.userId,
-            weight: props.weight,
-            date: new Date(props.date), 
-        };
-    }
+  InferAttributes<WeightHistory>,
+  InferCreationAttributes<WeightHistory>
+> {
+  declare id: CreationOptional<number>;
+  declare userId: ForeignKey<User["id"]>;
+  declare weight: number;
+  declare date: string;
+
+  public toJSON(): WeightHistoryEntity {
+    const props = super.get();
+    return {
+      id: props.id,
+      weight: props.weight,
+      date: new Date(props.date),
+    };
+  }
 }
 
 WeightHistory.init(
-    {
-        id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        userId: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        weight: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        date: {
-            type: Sequelize.DATE,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      tableName: TableNames.WeightHistory,
-      timestamps: false,
-      freezeTableName: true,
-    }
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: TableNames.WeightHistory,
+    timestamps: false,
+    freezeTableName: true,
+  }
 );
 
 export default WeightHistory;
