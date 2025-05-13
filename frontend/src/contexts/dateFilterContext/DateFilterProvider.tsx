3e88bd634ac3f083ac0b313442d9865e7e0dde68
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-05-13 00:05:37 -0300

    chore: movimentação da lógica do useDebounce para DateFilterContext

--- frontend/src/contexts/dateFilterContext/DateFilterProvider.tsx
+++ frontend/src/contexts/dateFilterContext/DateFilterProvider.tsx
@@ -2,10 +2,11 @@ import { PropsWithChildren, useCallback, useMemo, useState } from "react";
 import { DateFilterContext } from "./DateFilterContext";
 import { convertDateToLocalDateData } from "../../utils/helpers";
 import { DateIntervalType, PlainDate } from "../../@types";
+import { useDebounce } from "../../hooks/common/useDebounce";
 
 export function DateFilterProvider({ children }: PropsWithChildren) {
   const localDateData = useMemo(() => convertDateToLocalDateData(new Date()), []);
-
+  const { isDebouncing, startDebounce } = useDebounce(300);
   const [intervalType, setIntervalType] = useState<DateIntervalType>("monthly");
   const [dateData, setDateData] = useState<PlainDate>({
     year: localDateData.year,
@@ -14,14 +15,18 @@ export function DateFilterProvider({ children }: PropsWithChildren) {
     weekDay: localDateData.weekDay
   });
 
-  const updateDateDate = useCallback((date: Date) => {
-    setDateData({
-      year: date.getFullYear(),
-      month: date.getMonth(),
-      day: date.getDate(),
-      weekDay: date.getDay()
-    });
-  }, []);
+  const updateDateDate = useCallback(
+    (date: Date, withDebounce?: boolean) => {
+      if (withDebounce) startDebounce();
+      setDateData({
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        day: date.getDate(),
+        weekDay: date.getDay()
+      });
+    },
+    [startDebounce]
+  );
 
   const updateIntervalType = useCallback((type: DateIntervalType) => {
     setIntervalType(type);
@@ -29,7 +34,7 @@ export function DateFilterProvider({ children }: PropsWithChildren) {
 
   return (
     <DateFilterContext.Provider
-      value={{ dateData, intervalType, updateDateDate, updateIntervalType }}>
+      value={{ dateData, intervalType, isDebouncing, updateDateDate, updateIntervalType }}>
       {children}
     </DateFilterContext.Provider>
   );
