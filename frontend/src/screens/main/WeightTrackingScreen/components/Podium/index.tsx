43d7750c6aa43f6f51715dd45fed941fb94524b0
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-06-16 21:20:26 -0300

    refactor: atualização das legendas do podium

--- frontend/src/screens/main/WeightTrackingScreen/components/Podium/index.tsx
+++ frontend/src/screens/main/WeightTrackingScreen/components/Podium/index.tsx
@@ -1,42 +1,31 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
-import { dateToPTBR } from "../../../../../utils/helpers";
 import { PlanStrategy } from "../../../../../@core/entities/@shared/panStrategy/type";
 
 interface PodiumProps {
-  startWeight: {
-    date: Date;
-    value: number;
-  };
-  currentWeight: {
-    date: Date;
-    value: number;
-  };
-  targetWeight: {
-    date: Date;
-    value: number;
-  };
+  startWeight: number;
+  currentWeight: number;
+  targetWeight: number;
 }
 
 export function Podium(props: PodiumProps) {
   const { startWeight, currentWeight, targetWeight } = props;
 
-  const strategy: PlanStrategy =
-    targetWeight.value > startWeight.value ? "superavit" : "deficit";
+  const strategy: PlanStrategy = targetWeight > startWeight ? "superavit" : "deficit";
 
   const reachedTheGoal =
-    (strategy === "deficit" && currentWeight.value <= targetWeight.value) ||
-    (strategy === "superavit" && currentWeight.value >= targetWeight.value);
+    (strategy === "deficit" && currentWeight <= targetWeight) ||
+    (strategy === "superavit" && currentWeight >= targetWeight);
 
   return (
     <View style={styles.container}>
       <View style={[styles.place, styles.thirdPlace]}>
-        <Text style={styles.date}>{dateToPTBR(startWeight.date)}</Text>
-        <Text style={styles.value}>{startWeight.value} kg</Text>
+        <Text style={styles.value}>{startWeight} kg</Text>
+        <Text style={styles.label}>Inicial</Text>
       </View>
       <View style={[styles.place, styles.secondPlace]}>
-        <Text style={styles.date}>{dateToPTBR(currentWeight.date)}</Text>
-        <Text style={styles.value}>{currentWeight.value} kg</Text>
+        <Text style={styles.value}>{currentWeight} kg</Text>
+        <Text style={styles.label}>Atual</Text>
         {!reachedTheGoal && (
           <Image
             style={styles.runningAvocadoImage}
@@ -45,8 +34,8 @@ export function Podium(props: PodiumProps) {
         )}
       </View>
       <View style={[styles.place, styles.firstPlace]}>
-        <Text style={styles.date}>{dateToPTBR(targetWeight.date)}</Text>
-        <Text style={styles.value}>{targetWeight.value} kg</Text>
+        <Text style={styles.value}>{targetWeight} kg</Text>
+        <Text style={styles.label}>Objetivo</Text>
         <Image
           style={reachedTheGoal ? styles.finishFlagImageRight : styles.finishFlagImage}
           source={require("../../../../../assets/images/finish-flag.png")}
