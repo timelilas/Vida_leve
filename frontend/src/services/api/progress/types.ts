0744bb92427f0834a5c6ef2180cb4f825444687e
nalbert-cerqueira <nalbertc.p@gmail.com>
2025-08-15 21:00:08 -0300

    refactor: correção de alguma importações

--- frontend/src/services/api/progress/types.ts
+++ frontend/src/services/api/progress/types.ts
@@ -1,5 +1,5 @@
-import { ActitivyFrequency } from "../../@core/entities/@shared/activityFrequency/type";
-import { PlanType } from "../../@core/entities/@shared/planType/type";
+import { ActitivyFrequency } from "../../../@core/entities/@shared/activityFrequency/type";
+import { PlanType } from "../../../@core/entities/@shared/planType/type";
 
 export interface HttpUpsertProgressInputDTO {
   height: number;
